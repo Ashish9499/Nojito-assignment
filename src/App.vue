ac455b7682611cd0fd328fca0bad9f72ef975a4c
ASHISH VERMA <51405843+Ashish9499@users.noreply.github.com>
2022-05-29 17:26:28 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script>
+import loadingPage from "./components/Loading";
+export default {
+  components: {
+    loadingPage,
+  },
+};
+</script>
+
+<template>
+  <!-- <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div> -->
+  <loadingPage />
+  <router-view />
+</template>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Mulish:wght@400;500;700&display=swap");
+
+#app {
+  font-family: "Mulish", sans-serif, Helvetica, Arial;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
